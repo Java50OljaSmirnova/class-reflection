d086bc421eed8181ce95882af7c3db7f750be784
oljas <olja.smirnov93@gmail.com>
2023-11-29 22:17:06 +0200

    CW #49 method invocation application

---
+++ src/telran/reflect/Test.java
@@ -0,0 +1,23 @@
+package telran.reflect;
+
+public class Test {
+	void test1(int a) {
+		System.out.println("test1, parameter with value " + a);
+	}
+	void test2(int a) {
+		System.out.println("test2, parameter with value " + a);
+	}
+	void test3(int a) {
+		System.out.println("test3, parameter with value " + a);
+	}
+	void test14(int a) {
+		System.out.println("test4, parameter with value " + a);
+	}
+	void test51(int a) {
+		System.out.println("test5, parameter with value " + a);
+	}
+	void test6(int a) {
+		System.out.println("test6, parameter with value " + a);
+	}
+
+}
